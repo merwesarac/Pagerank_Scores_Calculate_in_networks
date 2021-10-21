13db8c78ebf4e38e9a4ca90ee0c79da2a996fbd2
merwesarac <mervesarac_18@hotmail.com>
2021-10-21 18:33:15 +0300

    Update Page_Rank.R

--- Page_Rank.R
+++ Page_Rank.R
@@ -3,13 +3,16 @@ edg<-read.table('veriseti.txt',header=TRUE)
 b<-sqldf('select distinct managerid from edg union select distinct employeeid from edg')
 nodeNum<-nrow(b)
 say<-data.frame()
+
 for(i in 1:nrow(b)){say<-rbind(say,nodeNum)}
    pr<-1/say
    pr<-data.frame(b,pr)
    pr<-data.frame(pr,say)
+
 colnames(pr)<-c("managerid","pr","nodeNum")
 weight<-sqldf('select edg.managerid,1.00/count(employeeid) as wei from edg group by edg.employeeid')
 edgWithOuterDegree<-sqldf('select edg.managerid,edg.employeeid,weight.wei from edg join weight on edg.managerid=weight.managerid')
+
 repeat
 {
      pagerank1<-sqldf('select edgWithOuterDegree.employeeid as src,sum(pr.pr*edgWithOuterDegree.wei*0.85) as pr
@@ -30,20 +33,3 @@ edg<-read.table('veriseti.txt',header=TRUE)
 manageremployeeid<-edg[,2:3]
 g<-graph.data.frame(manageremployeeid,directed=TRUE)
 page.rank (g, algo = c("power"),directed = TRUE, damping = 0.85,personalized = NULL, weights = NULL, options = NULL) 
-
-#1 level
-select name,employeeid,managerid,level,SYS_CONNECT_BY_PATH(name, '/') "Path",CONNECT_BY_ISCYCLE "Cycle",weight,imei,lokasyon
-from employee
-CONNECT BY NOCYCLE PRIOR employeeid=managerid and level<=1;
-
-#2 level
-select name,employeeid,managerid,level,SYS_CONNECT_BY_PATH(name, '/') "Path",CONNECT_BY_ISCYCLE "Cycle",weight,imei,lokasyon
-from employee
-CONNECT BY NOCYCLE PRIOR employeeid=managerid and level<=2;
-
-#3 level
-select name,employeeid,managerid,level,SYS_CONNECT_BY_PATH(name, '/') "Path",CONNECT_BY_ISCYCLE "Cycle",weight,imei,lokasyon
-from employee
-CONNECT BY NOCYCLE PRIOR employeeid=managerid and level<=3;
-
- 
